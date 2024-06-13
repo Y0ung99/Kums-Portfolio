bf78191a5fd5b8d10b09b4e6eac6cadf554e681b
KUM <ancolor@knou.ac.kr>
2024-06-13 19:07:07 +0900

    Javascript-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-const header = document.querySelector(".header");
-const headerHeight = header.getBoundingClientRect().height;
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
-  // 스크롤되는 Y좌표가 headerHeight보다 크다면 다른 스타일링
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
 });
+
+// Home섹션을 아래로 스크롤시 투명하게 처리
+const homeContainer = document.querySelector('.home__container');
+const homeHeight = homeContainer.offsetHeight;
+document.addEventListener('scroll', () => {
+  homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+})
